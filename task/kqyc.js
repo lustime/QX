8fd6c3fc56783b6104865b289ffe9fac14c43ad9
lustime <2247868358@qq.com>
2022-08-12 23:02:25 +0800

    Update kqyc.js

--- task/kqyc.js
+++ task/kqyc.js
@@ -1,7 +1,7 @@
 
 const lu_conf = $prefs.valueForKey('lus_conf');
 console.log(lu_conf);
-const conf = JSON.parse(lu_conf || '{}');
+const conf = lu_conf;
 const kqyc = conf.kqyc;
 const method = "POST";
 var date = new Date();
