010e7b4529d43eafb48a54bf9c461be563286985
lustime <2247868358@qq.com>
2022-08-12 22:45:08 +0800

    Create kqyc.js

---
+++ task/kqyc.js
@@ -0,0 +1,42 @@
+
+const lu_conf = $prefs.valueForKey('lus_conf');
+const conf = JSON.parse(lu_conf || '{}');
+const kqyc = conf.kqyc;
+const method = "POST";
+var date = new Date();
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+const first_day = year + '-' + month + '-' + '01';
+const last_day = year + '-' +month + '-' +day;
+const data = {
+    "command": "KQ-JG-002",
+    "params": {
+        "pageDTO": {
+            "pageSize": "100",
+            "pageNo": "1"
+        },
+        "startDate": first_day,
+        "endDate": last_day,
+        "displayType": "01",
+        "empId": conf.empId
+    }
+};
+
+const myRequest = {
+    url: kqyc.url,
+    method: method, // Optional, default GET.
+    headers: kqyc.headers, // Optional.
+    body: JSON.stringify(data) // Optional.
+};
+
+$task.fetch(myRequest).then(response => {
+    // response.statusCode, response.headers, response.body
+    console.log(response.body);
+    $notify("考勤异常", "", response.body); // Success!
+    $done();
+}, reason => {
+    // reason.error
+    $notify("Title", "Subtitle", reason.error); // Error!
+    $done();
+});
